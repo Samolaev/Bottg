bfb3c91e8974e2b6822665683fd20dc8391a4349
Samolaev <Gari5782014@gmail.com>
2026-01-17 16:44:44 +0300

    а

--- example/bot.py
+++ example/bot.py
@@ -2,7 +2,9 @@ from telegram import Update
 from telegram.ext import (
     Application,
     CommandHandler,
+    MessageHandler,
     ContextTypes,
+    filters,
 )
 from os import getenv
 
@@ -11,6 +13,9 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_html(text="hello world!")
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_html(text="help me!")
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(update.message.text)
 
 async def bot_tele(text):
     # Create application
@@ -21,12 +26,8 @@ async def bot_tele(text):
     # Add handlers
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
-    # Start application
-    await application.bot.set_webhook(url=getenv("webhook"))
-    await application.update_queue.put(
-            Update.de_json(data=text, bot=application.bot)
-        )
-    async with application:
-        await application.start()
-        await application.stop()
+    # Process the update
+    update = Update.de_json(data=text, bot=application.bot)
+    await application.process_update(update)
